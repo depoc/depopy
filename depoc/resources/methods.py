2b5f5364c5a758a2aba80f142936b6abd60ce939
Hugo Belém <belem.hugo@hotmail.com>
2025-02-23 09:38:31 -0300

    add filter method

--- depoc/resources/methods.py
+++ depoc/resources/methods.py
@@ -3,6 +3,35 @@ from typing import Any
 from .base import T, APIResource
 
 
+class Finder(APIResource[T]):
+    @classmethod
+    def filter(
+            cls,
+            search: str | None = None,
+            date: str | None = None,
+            start_date: str | None = None,
+            end_date: str | None = None,
+            limit: int | None = None,
+        ):
+        endpoint = f'{cls.endpoint}/?'
+
+        if search:
+            endpoint += f'search={search}'
+        
+        if date:
+            endpoint += f'date={date}'
+        elif date and search:
+            endpoint += f'&date={date}'
+
+        if start_date and end_date:
+            endpoint += f'start_date={start_date}&end_date={end_date}'
+        elif start_date and endpoint and search:
+            endpoint += f'&start_date={start_date}&end_date={end_date}'            
+
+        response = cls.requestor.request('GET', endpoint)
+        return cls._paginate(response, limit)
+
+
 class Create(APIResource[T]):
     @classmethod
     def create(cls, params: dict[str, Any]) -> T:
