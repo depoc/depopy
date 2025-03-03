9beb279f62edae1d9ede1f7454ff02238de3cc57
Hugo Belém <belem.hugo@hotmail.com>
2025-03-02 23:30:53 -0300

    publish v0.0.8

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ LONG_DESCRIPTION: str = (
 
 setup(
     name='depoc',
-    version='0.0.7',
+    version='0.0.8',
     description='Python bindings for the Depoc API',
     long_description=LONG_DESCRIPTION,
     author='Hugo Belém',
