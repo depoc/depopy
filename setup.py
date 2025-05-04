d4487fef312a0d05a0c33a1715270fbdc9b6b523
Hugo Belém <belem.hugo@hotmail.com>
2025-05-04 19:17:56 -0300

    patch: bug fix v0.1.4a2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ LONG_DESCRIPTION: str = (
 
 setup(
     name='depoc',
-    version='0.1.4a2', 
+    version='0.1.4a2',
     description='Python bindings for the Depoc API',
     long_description=LONG_DESCRIPTION,
     author='Hugo Belém',
